7c637d46e73cb73549ed261347e5e1a01a8dc2bb
Phil W <phil.williams@advisoft.co.nz>
2025-07-07 17:46:50 +1200

    Initial blank help site

---
+++ content/_index.md
@@ -0,0 +1,3 @@
+---
+title: "Mainline Dyno Help"
+---
---
+++ public/searchindex.en.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Mainline Dyno Help",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
